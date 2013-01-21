140b9bffce23b62c490ef05e97154c2645633c23
Jim Brusstar <jim.brusstar@gmail.com>
2013-01-21 15:35:37 -0800

    Slight simplification.

--- haversine.js
+++ haversine.js
@@ -8,7 +8,7 @@ var EARTH_RADIUS_KILOMETERS = 6371;
 /**
  * Convert degrees to radians.
  */
-var toRad = function(n) {
+var degToRad = function(n) {
   return n * Math.PI / 180;
 };
 
@@ -35,17 +35,15 @@ module.exports = function(start, end, units) {
   var endLat = end.latitude || end.lat;
   var endLng = end.longitude || end.lng;
 
-  var R = (units === 'km') ? EARTH_RADIUS_KILOMETERS : EARTH_RADIUS_MILES;
+  var radius = (units === 'km') ? EARTH_RADIUS_KILOMETERS : EARTH_RADIUS_MILES;
 
-  var dLat = toRad(endLat - startLat);
-  var dLon = toRad(endLng - startLng);
-  var lat1 = toRad(startLat);
-  var lat2 = toRad(endLat);
+  var dLat = degToRad(endLat - startLat);
+  var dLng = degToRad(endLng - startLng);
 
   var a =
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLng / 2) * Math.sin(dLng / 2) *
+    Math.cos(degToRad(startLat)) * Math.cos(degToRad(endLat));
 
-  return R * c;
+  return 2 * radius * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 };
