2fb07ff48f3258a3d3dc1d1184b528611b1a108d
Jim Brusstar <jim.brusstar@gmail.com>
2013-01-21 14:40:30 -0800

    Un-nest functions in haversine.

--- haversine.js
+++ haversine.js
@@ -1,36 +1,31 @@
 'use strict';
 
-// haversine
-// By Nick Justice (niix)
-// https://github.com/niix/haversine
+/**
+ * Convert degrees to radians.
+ */
+var toRad = function(n) {
+  return n * Math.PI / 180;
+};
 
-var haversine = (function() {
+/**
+ * Haversine.
+ */
+module.exports = function(start, end, options) {
+  var miles = 3960;
+  var km    = 6371;
+  options   = options || {};
 
-  // convert to radians
-  var toRad = function(num) {
-    return num * Math.PI / 180;
-  };
+  var R = options.unit === 'km' ? km : miles;
 
-  return function haversine(start, end, options) {
-    var miles = 3960;
-    var km    = 6371;
-    options   = options || {};
+  var dLat = toRad(end.latitude - start.latitude);
+  var dLon = toRad(end.longitude - start.longitude);
+  var lat1 = toRad(start.latitude);
+  var lat2 = toRad(end.latitude);
 
-    var R = options.unit === 'km' ? km : miles;
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-    var dLat = toRad(end.latitude - start.latitude);
-    var dLon = toRad(end.longitude - start.longitude);
-    var lat1 = toRad(start.latitude);
-    var lat2 = toRad(end.latitude);
-
-    var a =
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-    return R * c;
-  };
-
-})();
-
-module.exports = haversine;
+  return R * c;
+};
