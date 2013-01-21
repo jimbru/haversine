95b1c995dbac124e1a68880517cbb60c9893d1c0
Jim Brusstar <jim.brusstar@gmail.com>
2013-01-21 14:17:59 -0800

    Mocha Unit Testing.
    Upgraded unit test to use Mocha. Expanded on the test itself. Modified
    lint config to avoid choking on Mocha globals. Set npm test script and
    set lint to run pre-test.

--- test/test.js
+++ test/test.js
@@ -1,22 +1,37 @@
+'use strict';
+
+/**
+ * Haversine Test
+ * Designed to be run under Mocha.
+ */
+var assert = require('assert');
 var haversine = require('../haversine');
 
-var start = {
-  latitude: 50.03,
-  longitude: 5
-};
+var sf = {latitude: 37.783333, longitude: -122.416667};
+var nyc = {latitude: 40.664167, longitude: -73.938611};
+var syd = {latitude: -33.859972, longitude: 151.211111};
 
-var end = {
-  latitude: 58.38,
-  longitude: 3
-};
+describe('Haversine', function() {
+  it('should return zero when start equals end', function() {
+    var point = {latitude: 54.321, longitude: 123.45};
+    assert.strictEqual(haversine(point, point), 0);
+  });
 
-// inserting values directly
-console.log(
-  haversine({latitude: 12, longitude: 11}, {latitude: 10, longitude: 10})
-);
+  it('should satisfy the commutative property', function() {
+    assert.strictEqual(haversine(sf, nyc), haversine(nyc, sf));
+  });
 
-// using objects
-console.log(haversine(start, end));
+  it('should use miles by default', function() {
+    assert.strictEqual(haversine(sf, nyc), haversine(sf, nyc, {unit: 'mile'}));
+  });
 
-// using objects with unit conversion
-console.log(haversine(start, end, {unit: 'km'}));
+  it('should return the correct distance from SF to NYC', function() {
+    assert.strictEqual(Math.floor(haversine(sf, nyc)), 2570);
+    assert.strictEqual(Math.floor(haversine(sf, nyc, {unit: 'km'})), 4135);
+  });
+
+  it('should return the correct distance from SF to SYD', function() {
+    assert.strictEqual(Math.floor(haversine(sf, syd)), 7426);
+    assert.strictEqual(Math.floor(haversine(sf, syd, {unit: 'km'})), 11947);
+  });
+});
